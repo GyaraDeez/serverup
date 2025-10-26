c4528b409988e5bfe82151a225014860672cd094
GamerDoop7 <145089422+GyaraDeez@users.noreply.github.com>
2025-10-26 21:21:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -517,3 +517,11 @@ function main(){
 main();
 // --- Start the bot ---
 
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Keep-alive server active.');
+});
+
+
